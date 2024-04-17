2eab3984f8e8cf850ff867daeeba6f0a532cdf43
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 04:49:05 -0500

    feat (main.jsx): carga de componente PokemonApp
    Se carga como componente inicial del app PokemonAPP para aplicar conceptos de carga asíncrona

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
-import './index.css';
 
+import { PokemonApp } from './PokemonApp.jsx';
 import { store } from './store';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 );
