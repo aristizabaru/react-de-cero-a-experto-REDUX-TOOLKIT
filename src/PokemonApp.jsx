55b3ced468e1c0262c2080ccd0650eafbcb8c9df
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 04:49:57 -0500

    first commit

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,14 @@
+export const PokemonApp = () => {
+    return (
+        <>
+            <h1>Pokemon App</h1>
+            <hr />
+            <ul>
+                <li>Hola</li>
+                <li>Hola</li>
+                <li>Hola</li>
+                <li>Hola</li>
+            </ul>
+        </>
+    );
+};
