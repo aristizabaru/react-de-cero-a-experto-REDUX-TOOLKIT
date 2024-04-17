3753ce9acc61c109509ee42c2e3bb6f28b28855b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 04:50:58 -0500

    feat (counterSlice.js): implementación de slice del contador
    Se implementa el slice del contador adicionando reducers para el incremento, decremento e incremento por número pasado como parámetro

--- src/store/slice/counter/counterSlice.js
+++ src/store/slice/counter/counterSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    counter: 10,
-};
-
 export const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: {
+        counter: 10,
+    },
     reducers: {
         increment: (state) => {
             // Redux Toolkit allows us to write "mutating" logic in reducers. It
@@ -15,8 +13,14 @@ export const counterSlice = createSlice({
             // immutable state based off those changes
             state.counter += 1;
         },
+        decrement: (state) => {
+            state.counter -= 1;
+        },
+        incrementBy: (state, action) => {
+            state.counter += action.payload;
+        },
     },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy } = counterSlice.actions;
