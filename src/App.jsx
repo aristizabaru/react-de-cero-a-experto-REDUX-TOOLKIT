43480a689a95291ea8e720e7a6a3ffce5da8f202
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 04:49:48 -0500

    feat (app.jsx): implementación de counter
    Se implementa un counter usando Redux para gestionar el estado del mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+
+import { decrement, increment, incrementBy } from './store/slice/counter';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -16,20 +20,20 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => { dispatch(increment()); }}>
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => { dispatch(decrement()); }}>
+          Decrement
+        </button>
+        <button onClick={() => { dispatch(incrementBy(2)); }}>
+          Increment by 2
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
