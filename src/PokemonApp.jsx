5d147ef29aeb670875ca9d7e2e2b2e836464480e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 06:50:12 -0500

    feat (pokemonApp.jsx): implementación de compomente con redux
    Se implementa componente para paginación de pokemones, el estado global se gestiona mediante redux

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,14 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slice/pokemon";
+
 export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+    const { isLoading, pokemons = [], page } = useSelector(state => state.pokemons);
+
+    useEffect(() => {
+        dispatch(getPokemons());
+    }, [dispatch]);
+
+
     return (
         <>
             <h1>Pokemon App</h1>
             <hr />
+            <span>Loading: {isLoading ? 'True' : 'False'}</span>
             <ul>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
-                <li>Hola</li>
+                {pokemons.map(({ name }) => (
+                    <li key={name}>{name}</li>
+                ))}
             </ul>
+            <button
+                disabled={isLoading}
+                onClick={() => { dispatch(getPokemons(page)); }}
+            >
+                Next
+            </button>
         </>
     );
 };
