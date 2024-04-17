1b8573d66677f1458a3458578185842935a9aba4
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-04-17 04:46:25 -0500

    feat (store.js): se adiciona slide de pokemon al store para gestión del api

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slice/counter';
+import { pokemonSlice } from './slice/pokemon';
 
 export const store = configureStore({
     reducer: {
-        counter: counterSlice.reducer
+        counter: counterSlice.reducer,
+        pokemons: pokemonSlice.reducer,
     },
 });
